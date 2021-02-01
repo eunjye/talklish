07bba95f7120ceb0cb6000543f6372e48e57e98f
eunjye <eunjh1220@naver.com>
2021-02-01 17:59:43 +0900

    working

--- speak_up/js/common.js
+++ speak_up/js/common.js
@@ -35,7 +35,6 @@
 			var $targetPage = document.querySelector('[data-index="' + targetIndex + '"]');
 			if (!!$evtEl) {
 				var $evtPage = $evtEl.closestByClass('page-area');
-				console.log($evtPage);
 				$evtPage.style.display = 'none';
 			}
 			$targetPage.style.display = 'block';
@@ -105,7 +104,7 @@
 		},
 		/**
 		 * 
-		 * @param {Object} speak {text:String, voice:String, duration:Number, endBack:function}
+		 * @param {Object} script {text:String, voice:String, duration:Number, endBack:function}
 		 * @param {Object} question {type:String, answer:Array}
 		 */
 		askQuestion: function(script, question){
@@ -116,113 +115,178 @@
 			};
 
 			win[namespace].setText(text);
-			win[namespace].soundStatus('play', 'script', voice, fnEndBack);
 
-			if (question !== undefined){
-				setTimeout(function(){
-					win[namespace].checkAnswer(script, question);
-				}, script.duration);
+			// 질문이 있을때는 checkAnswer로 넘어감
+			if (question === undefined){
+				win[namespace].soundStatus('play', 'script', voice, fnEndBack);
+			} else {
+				console.log('!!!');
+				win[namespace].soundStatus('play', 'script', voice);
+				win[namespace].checkAnswer(script, question, fnEndBack);
+				// fnEndBack();
 			}
 		},
-		wrongAnswer: function(script){
-			var text = script.text;
-			var voice = script.voice;
+		/**
+		 * 
+		 * @param {Object} wrongScript  틀렸다는 음성 & 지문
+		 * @param {function} endBack 
+		 */
+		wrongAnswer: function(wrongScript, endBack){
+			var text = wrongScript.text;
+			var voice = wrongScript.voice;
+			var duration = wrongScript.duration;
 
 			win[namespace].setText(text);
-			win[namespace].soundStatus('play', 'wrong', voice);
 			document.querySelector('.question-area').style.display = 'none';
+			win[namespace].soundStatus('play', 'wrong', voice);
+			win[namespace].checkAnswerTry++;
+
+			setTimeout(function(){
+				document.querySelector('.question-area').style.display = 'block';
+				endBack();
+			}, duration)
 		},
 		setText: function(text){
 			var $text = document.querySelector('.txt-script span');
 			$text.innerText = text;
 		},
-		checkAnswer: function(script, question){
+		/**
+		 * 
+		 * @param {} script 
+		 * @param {Object} question 
+				{
+					type: 'word',
+					answer: [
+						['ㄱㅈ'],
+						['고지', '고증', '고장'],
+						['고장']
+					]
+				};
+		 * @param {*} endBack 
+		 */
+		checkAnswerTry: 1,
+		checkAnswer: function(script, question, endBack){
 			var $questionArea = document.querySelector('.question-area');
+			var $btnVoide = document.querySelector('.btn-voice');
 
+
+			// [QuizType1] 단답형일 시
 			if (question.type === 'word') {
-				// answer check !! (question.question[0])
+				// answer check !! (question.answer[0])
 				$questionArea.style.display='block';
 				$questionArea.innerHTML = '<div class="single"></div>';
 				var $questionInner = $questionArea.querySelector('.single');
-				
-				// 첫번째 녹음하기
-        if (externalManager.isPlayer() || true) { // [to-be] true는 나중에 빼야함
-					var answerWord = question.answer[2][0];
-					var initialAnswer = question.answer[0][0];
-					var reduceAnswerWord = answerWord.replace(/(\s*)/g,'');
-					var answerLength = reduceAnswerWord.length;
+				var answerText = question.answer[2][0];
+				var reduceAnswerText = answerText.replace(/ /g,'');
 
-					var voice = getVoice();
-					if (voice.reduceText.substring(0, answerLength) === reduceAnswerWord) {
-						// 맞았을 때 (1트)
-						setInitialAnswer(answerWord);
-					} else {
-						// 틀렸을 때 (1트)
-						win[namespace].wrongAnswer(win[namespace].wrongSpeak[0]); // [to-be] 이거 랜덤으로 해야됨
-
-						setTimeout(function(){
-							setInitialAnswer(initialAnswer);
-							$questionArea.style.display='block';
-	
-							win[namespace].askQuestion(script);
-						}, win[namespace].wrongSpeak[0].duration);
-					}
-
-					// 초성값 .single에 넣기
-					function setInitialAnswer(initial) {
-						var resultHTML = '';
-						for(var i = 0; i < initial.length; i++) {
-							if (initial[i] === ' ') {
-								resultHTML += '&nbsp;';
+				// 초성값 .single에 넣기
+				function setInitialAnswer(initial, isTransparent) {
+					var resultHTML = '';
+					for(var i = 0; i < initial.length; i++) {
+						if (initial[i] === ' ') {
+							resultHTML += '&nbsp;';
+						} else {
+							if (isTransparent) {
+								resultHTML += '<span></span>';
 							} else {
 								resultHTML += '<span>' + initial[i] + '</span>';
 							}
 						}
-						$questionInner.innerHTML = resultHTML;
 					}
-
-					// 음성인식 실행해서 값 가져오기
-					function getVoice(){
-						var str = '음 디지털 영상 지도 아니니'; // 테스트용
-
-						// window.HybridApp.startSilvySTTMode(0); // 여기 풀기
-						// window.HybridApp.onResultSTTMode = function(str) { // 여기 풀기
-							return {text: str, reduceText: str.replace(/(\s*)/g,'')};
-						// } // 여기 풀기
-					}
-				} else {
-					setSilvyText("버리야 안녕 나는 씩씩한 냥이야. 도덕 공부를 하고 있어.");
+					$questionInner.innerHTML = resultHTML;
 				}
 
+				// 첫번째 (글자수만 나오는거)
+				setInitialAnswer(answerText, true);
+				
+				$btnVoide.addEventListener('click', function(){
+					var voiceText = getVoice();
+					var tryNum = win[namespace].checkAnswerTry;
+					
+					if (voiceText.reduceText.slice(0, reduceAnswerText.length) !== reduceAnswerText) {
+						
+						// 1트에 실패일 시, 초성 나옴
+						if (tryNum === 1){
+							win[namespace].setText(win[namespace].wrongScript[0].text);
+							win[namespace].wrongAnswer(
+								win[namespace].wrongScript[0], 
+								function(){
+									setInitialAnswer(question.answer[0][0]);
+									win[namespace].soundStatus('play', 'script', script.voice);
+									win[namespace].setText(script.text);
+								}
+							); // [To-be] 여기 랜덤으로
+						} else if (tryNum === 2){
+							win[namespace].setText(win[namespace].wrongScript[1].text);
+							win[namespace].wrongAnswer(
+								win[namespace].wrongScript[1], 
+								function(){
+									setInitialAnswer(question.answer[1][0]); // not single 
+									win[namespace].soundStatus('play', 'script', script.voice);
+									win[namespace].setText(script.text);
+								}
+							); // [To-be] 여기 랜덤으로
+						} else if (tryNum === 3){
+							win[namespace].setText(win[namespace].wrongScript[2].text);
+							win[namespace].wrongAnswer(// [To-be] wrongAnswer()가 아니라, 으음~아쉬워 알려줄게! 랜덤으로
+								win[namespace].wrongScript[2], 
+								function(){ 
+								}
+							);
+							setInitialAnswer(answerText);
+							win[namespace].checkAnswerTry = 1;
+							!!endBack && endBack();
+						}
+					} else {
+						// 성공일 시, 정답
+						setInitialAnswer(answerText);
+						win[namespace].checkAnswerTry = 1;
+						!!endBack && endBack();
+					}
+
+				});
+
 			} else if (question.type === 'ox') {
+				// [QuizType2] O/X일 시
 
 			}
+
+			
+			// 음성인식 실행해서 값 가져오기
+			function getVoice(){
+				var str = '고장 같아'; // 테스트용
+
+				// window.HybridApp.startSilvySTTMode(0); // 여기 풀기
+				// window.HybridApp.onResultSTTMode = function(str) { // 여기 풀기
+					return {text: str, reduceText: str.replace(/(\s*)/g,'')};
+				// } // 여기 풀기
+			}
 		},
-		wrongSpeak: [
+		wrongScript: [
 			{
 				text: '다시 한번 생각해보자!',
 				voice: 'SSJ3b_A_01',
-				duration: 5800
+				duration: 3000
 			},
 			{
 				text: '조금 더 생각해보자!',
 				voice: 'SSJ3b_A_02',
-				duration: 5800
+				duration: 3000
 			},
 			{
 				text: '한 번 더 생각해볼까?',
 				voice: 'SSJ3b_A_03',
-				duration: 5800
+				duration: 3000
 			},
 			{
 				text: '글쎄, 한 번 더 생각해봐!',
 				voice: 'SSJ3b_A_04',
-				duration: 5800
+				duration: 3000
 			},
 			{
 				text: '아쉬워~ 한 번 더 생각해볼까?',
 				voice: 'SSJ3b_A_05',
-				duration: 5800
+				duration: 3000
 			},
 		],
 		speak: [
@@ -264,9 +328,9 @@
 							{
 								type: 'word',
 								answer: [
-									['ㄷㅈㅌ ㅇㅅ ㅈㄷ'],
-									['디지털 영상 지도', '디지털 인식 지도', '디지털 안심 지도'],
-									['디지털 영상 지도']
+									['ㄱㅈ'],
+									['고지', '고증', '고장'],
+									['고장']
 								]
 							});
 					}
@@ -281,7 +345,10 @@
 				},
 				{
 					text: '사람들이 모여 사는 곳을 고장이라고 해.',
-					voice: 'SSJ310107_06'
+					voice: 'SSJ310107_06',
+					endBack: function(){
+						win[namespace].askQuestion(win[namespace].speak[1][3]);
+					}
 				},
 				{
 					text: '우리 고장의 여러 장소가 머릿속에 떠오르네!',
