0b2c6dc4a5365c23f9c69f6eb2e5f74c0c8b7f39
eunjye <eunjh1220@naver.com>
2021-02-02 11:21:22 +0900

    voice add

--- speak_up/js/common.js
+++ speak_up/js/common.js
@@ -26,6 +26,7 @@
 	var namespace = 'speakUp';
 
 	win[namespace] = {
+		willTimer: null,
 		/**
 		 * @name goToNPage
 		 * @param {Number} targetIndex 
@@ -48,18 +49,21 @@
 		/**
 		 * 
 		 * @param {String} status (stop, play)
-		 * @param {String} type (bgm, voice, script, effect) 
+		 * @param {String} type (bgm, wrong, script, effect) 
 		 * @param {String} name (intro, 01, 02 ...) 
 		 * @param {function} callback
 		 */
 		soundStatus: function(status, type, name, callback){
-			
+			var soundType = type;
+			if (type === 'wrong' || type === 'script') {
+				soundType = 'script';
+			}
 			if (status === 'play') { // play status
 				if (!document.querySelector('.' + name)) { // no have bgm tag
 					var $audio = document.createElement('audio');
 					var url = 'audio/' + type + '/' + name + '.mp3';
 					$audio.setAttribute('class', name);
-					$audio.classList.add('class', 'audio-' + type);
+					$audio.classList.add('class', 'audio-' + soundType);
 					$audio.setAttribute('src', url);
 					$audio.setAttribute('name', 'audio/mpeg');
 					if (type === 'bgm') {
@@ -85,7 +89,7 @@
 			}
 
 			function muteByType(type) {
-				var $otherAudio = document.querySelectorAll('[class*=audio-' + type + ']');
+				var $otherAudio = document.querySelectorAll('[class*=audio-' + soundType + ']');
 				var otherAudioindex = 0;
 				while (otherAudioindex < $otherAudio.length) {
 					$otherAudio[otherAudioindex].pause();
@@ -111,18 +115,18 @@
 			var text = script.text;
 			var voice = script.voice;
 			var fnEndBack = function(){
-				setTimeout(script.endBack, script.duration)
+				win[namespace].willTimer = setTimeout(script.endBack, script.duration)
 			};
 
 			win[namespace].setText(text);
 
 			// 질문이 있을때는 checkAnswer로 넘어감
+			clearTimeout(win[namespace].willTimer);
 			if (question === undefined){
 				win[namespace].soundStatus('play', 'script', voice, fnEndBack);
 			} else {
-				console.log('!!!');
 				win[namespace].soundStatus('play', 'script', voice);
-				win[namespace].checkAnswer(script, question, fnEndBack);
+				win[namespace].checkAnswer(script, question, script.endBack);
 				// fnEndBack();
 			}
 		},
@@ -141,7 +145,8 @@
 			win[namespace].soundStatus('play', 'wrong', voice);
 			win[namespace].checkAnswerTry++;
 
-			setTimeout(function(){
+			clearTimeout(win[namespace].willTimer);
+			win[namespace].willTimer = setTimeout(function(){
 				document.querySelector('.question-area').style.display = 'block';
 				endBack();
 			}, duration)
@@ -198,18 +203,20 @@
 
 				// 첫번째 (글자수만 나오는거)
 				setInitialAnswer(answerText, true);
-				
-				$btnVoide.addEventListener('click', function(){
+				$btnVoide.addEventListener('click', startVoiceCheck);
+
+				function startVoiceCheck(){
 					var voiceText = getVoice();
 					var tryNum = win[namespace].checkAnswerTry;
+					win[namespace].soundStatus('stop', 'script');
 					
 					if (voiceText.reduceText.slice(0, reduceAnswerText.length) !== reduceAnswerText) {
 						
 						// 1트에 실패일 시, 초성 나옴
 						if (tryNum === 1){
-							win[namespace].setText(win[namespace].wrongScript[0].text);
+							win[namespace].setText(win[namespace].wrongScript[0][0].text);
 							win[namespace].wrongAnswer(
-								win[namespace].wrongScript[0], 
+								win[namespace].wrongScript[0][0], 
 								function(){
 									setInitialAnswer(question.answer[0][0]);
 									win[namespace].soundStatus('play', 'script', script.voice);
@@ -217,9 +224,9 @@
 								}
 							); // [To-be] 여기 랜덤으로
 						} else if (tryNum === 2){
-							win[namespace].setText(win[namespace].wrongScript[1].text);
+							win[namespace].setText(win[namespace].wrongScript[0][1].text);
 							win[namespace].wrongAnswer(
-								win[namespace].wrongScript[1], 
+								win[namespace].wrongScript[0][1], 
 								function(){
 									setInitialAnswer(question.answer[1][0]); // not single 
 									win[namespace].soundStatus('play', 'script', script.voice);
@@ -227,24 +234,25 @@
 								}
 							); // [To-be] 여기 랜덤으로
 						} else if (tryNum === 3){
-							win[namespace].setText(win[namespace].wrongScript[2].text);
 							win[namespace].wrongAnswer(// [To-be] wrongAnswer()가 아니라, 으음~아쉬워 알려줄게! 랜덤으로
-								win[namespace].wrongScript[2], 
+								win[namespace].wrongScript[1][2], 
 								function(){ 
+									!!endBack && endBack();
 								}
 							);
+							win[namespace].setText(win[namespace].wrongScript[1][2].text);
 							setInitialAnswer(answerText);
 							win[namespace].checkAnswerTry = 1;
-							!!endBack && endBack();
+							$btnVoide.removeEventListener('click', startVoiceCheck);
 						}
 					} else {
-						// 성공일 시, 정답
+						// 성공일 시, 정답 모션
 						setInitialAnswer(answerText);
 						win[namespace].checkAnswerTry = 1;
 						!!endBack && endBack();
+						$btnVoide.removeEventListener('click', startVoiceCheck);
 					}
-
-				});
+				}
 
 			} else if (question.type === 'ox') {
 				// [QuizType2] O/X일 시
@@ -254,7 +262,7 @@
 			
 			// 음성인식 실행해서 값 가져오기
 			function getVoice(){
-				var str = '고장 같아'; // 테스트용
+				var str = '음 고장 같아'; // 테스트용
 
 				// window.HybridApp.startSilvySTTMode(0); // 여기 풀기
 				// window.HybridApp.onResultSTTMode = function(str) { // 여기 풀기
@@ -263,31 +271,50 @@
 			}
 		},
 		wrongScript: [
-			{
-				text: '다시 한번 생각해보자!',
-				voice: 'SSJ3b_A_01',
-				duration: 3000
-			},
-			{
-				text: '조금 더 생각해보자!',
-				voice: 'SSJ3b_A_02',
-				duration: 3000
-			},
-			{
-				text: '한 번 더 생각해볼까?',
-				voice: 'SSJ3b_A_03',
-				duration: 3000
-			},
-			{
-				text: '글쎄, 한 번 더 생각해봐!',
-				voice: 'SSJ3b_A_04',
-				duration: 3000
-			},
-			{
-				text: '아쉬워~ 한 번 더 생각해볼까?',
-				voice: 'SSJ3b_A_05',
-				duration: 3000
-			},
+			[
+				{
+					text: '다시 한번 생각해보자!',
+					voice: 'SSJ3b_A_01',
+					duration: 3000
+				},
+				{
+					text: '조금 더 생각해보자!',
+					voice: 'SSJ3b_A_02',
+					duration: 3000
+				},
+				{
+					text: '한 번 더 생각해볼까?',
+					voice: 'SSJ3b_A_03',
+					duration: 3000
+				},
+				{
+					text: '글쎄, 한 번 더 생각해봐!',
+					voice: 'SSJ3b_A_04',
+					duration: 3000
+				},
+				{
+					text: '아쉬워~ 한 번 더 생각해볼까?',
+					voice: 'SSJ3b_A_05',
+					duration: 3000
+				},
+			],
+			[
+				{
+					text: '아쉬워~ 내가 설명해 줄게!',
+					voice: 'SSJ3b_B_01',
+					duration: 4000
+				},
+				{
+					text: '잘 모르겠다면 내가 알려줄게!',
+					voice: 'SSJ3b_B_02',
+					duration: 4000
+				},
+				{
+					text: '어려웠구나! 내가 알려줄게.',
+					voice: 'SSJ3b_B_03',
+					duration: 4000
+				},
+			]
 		],
 		speak: [
 			[
@@ -346,35 +373,70 @@
 				{
 					text: '사람들이 모여 사는 곳을 고장이라고 해.',
 					voice: 'SSJ310107_06',
+					duration: 4500,
 					endBack: function(){
 						win[namespace].askQuestion(win[namespace].speak[1][3]);
 					}
 				},
 				{
 					text: '우리 고장의 여러 장소가 머릿속에 떠오르네!',
-					voice: 'SSJ310107_07'
+					voice: 'SSJ310107_07',
+					duration: 5000,
+					endBack: function(){
+						win[namespace].askQuestion(win[namespace].speak[2][0]);
+					}
 				},
 			],
 			[
 				{
 					text: '그 중에서, 내가 제일 좋아하는 장소가 어디인지 맞춰볼래?',
-					voice: 'SSJ310107_08'
+					voice: 'SSJ310107_08',
+					duration: 5800,
+					endBack: function(){
+						// 여기서 박스 나타남
+						win[namespace].askQuestion(
+							win[namespace].speak[2][1],
+							{
+								type: 'word',
+								answer: [
+									['ㄱㅈ'],
+									['고지', '고증', '고장'],
+									['고장']
+								]
+							}
+						);
+					}
 				},
 				{
 					text: '이곳은, 친구들과 함께 교실에서 공부하고 운동장에서 재미있게 놀 수 있는 곳이야.',
-					voice: 'SSJ310107_09'
+					voice: 'SSJ310107_09',
+					duration: 6000,
+					endBack: function(){
+						win[namespace].askQuestion(win[namespace].speak[2][2]);
+					}
 				},
 				{
 					text: '친구들과 함께 교실에서 공부하고 운동장에서 재미있게 노는 곳은 학교이지.',
-					voice: 'SSJ310107_10'
+					voice: 'SSJ310107_10',
+					duration: 6500,
+					endBack: function(){
+						win[namespace].askQuestion(win[namespace].speak[2][3]);
+					}
 				},
 				{
 					text: '나중에 우리 고장의 장소 알림판에 학교를 꼭 소개할 테야. ',
-					voice: 'SSJ310107_11'
+					voice: 'SSJ310107_11',
+					duration: 5500,
+					endBack: function(){
+						win[namespace].askQuestion(win[namespace].speak[2][4]);
+					}
 				},
 				{
 					text: '그때 꼭 도와줘야해! 알겠지?',
-					voice: 'SSJ310107_12'
+					voice: 'SSJ310107_12',
+					duration: 4000,
+					endBack: function(){
+					}
 				},
 			]
 		],
